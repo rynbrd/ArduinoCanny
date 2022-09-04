1dab897885ea5780bed0533f7db659f9dfc28f7e
Ryan Bourgeois <bluedragonx@gmail.com>
2022-09-04 00:42:45 -0700

    Remove debug prints.

--- src/Canny/RealDash.cpp
+++ src/Canny/RealDash.cpp
@@ -171,10 +171,6 @@ void RealDash::writeBytes(const byte* b, uint8_t len) {
 }
 
 void RealDash::writeBytes(uint32_t data) {
-    //uint8_t* buf = (uint8_t*)&data;
-    //for (int i = 3; i >= 0; --i) {
-    //    writeByte(buf[i]);
-    //}
     writeBytes((const byte*)&data, 4);
 }
 
@@ -185,18 +181,6 @@ Error RealDash::write(uint32_t id, uint8_t ext, uint8_t* data, uint8_t size) {
     if (size > 64 || size % 4 != 0) {
         return ERR_INVALID;
     }
-
-    uint8_t test_data[] = {
-        0x66, 0x33, 0x22, 0x11, 0x20, 0x00, 0x00, 0x00,
-        0x0E, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    };
-
-    Serial.println("----");
-    Serial.print("rd checksum: ");
-    Serial.println(Crc32(test_data, 16), HEX);
-    Serial.print("my checksum: ");
-    Serial.println(CRC32::calculate(test_data, 16), HEX);
-    Serial.println("----");
 
     write_checksum_.reset();
 
@@ -213,14 +197,12 @@ Error RealDash::write(uint32_t id, uint8_t ext, uint8_t* data, uint8_t size) {
         writeByte(0);
     }
     uint32_t checksum = write_checksum_.value();
-    Serial.print("realdash checksum: "); Serial.println(checksum, HEX);
     stream_->write((const byte*)&checksum, 4);
     stream_->flush();
     return Error::ERR_OK;
 }
 
 Error RealDash::write(const Frame& frame) {
-    Serial.print("realdash frame: "); Serial.println(frame);
     return write(frame.id(), frame.ext(), frame.data(), frame.size());
 }
 
